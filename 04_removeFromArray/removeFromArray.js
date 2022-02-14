f1b5478ed13d1a38f7575c7ed22e0f2ff9eb4331
Crekto <hiimCrekto@yahoo.com>
2022-02-14 17:14:29 +0200

    Added solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
 const removeFromArray = function() {
-
+    let array = arguments[0];
+    let aux = [];
+    array.forEach(el => {
+        let ok = 1;
+        for(let i = 1; i < arguments.length; i++)
+            if(el === arguments[i])
+                ok = 0;
+        if(ok == 1)
+            aux.push(el);
+    });
+    return aux;
 };
 
 // Do not edit below this line
