b3543878338e702c3ab740ac9f154e8b8ba0c1f4
Crekto <hiimCrekto@yahoo.com>
2022-02-14 16:27:27 +0200

    Added solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, numberOfRepeats) {
+    if(numberOfRepeats == 0)
+        return "";
+    if(numberOfRepeats < 0)
+        return "ERROR";
+    aux = string;
+    for(let i = 1; i < numberOfRepeats; i++)
+        string = string + aux;
+    return string;
 };
 
 // Do not edit below this line
