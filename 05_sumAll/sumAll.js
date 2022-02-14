0359225eade60713495c908c2f3f245746e8c0d7
Crekto <hiimCrekto@yahoo.com>
2022-02-14 17:24:08 +0200

    Added solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if(a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b))
+        return "ERROR";
+    let sum = 0;
+    if(a > b)
+    {
+        aux = a;
+        a = b;
+        b = aux;
+    }
+    for(let i = a; i <= b ; i++)
+        sum += i;
+    return sum;
 };
 
 // Do not edit below this line
