2182d44ae20e193e89c754c75e2e8f52faf11f65
Crekto <hiimCrekto@yahoo.com>
2022-02-14 16:41:27 +0200

    Added solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let aux = "";
+    for(let i = 0; i < string.length; i++)
+        aux += string[string.length - i - 1];
+        
+    return aux;
 };
 
 // Do not edit below this line
